182736b6f597f631582438776de030c990fe7731
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-11-22 13:19:59 -0300

    feat: Update migrations

--- src/database/migrations/1605741100304-CreateAppointments.ts
+++ src/database/migrations/1605741100304-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1605741100304
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1606058647590-CreateUsers.ts
+++ src/database/migrations/1606058647590-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1606058647590 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
