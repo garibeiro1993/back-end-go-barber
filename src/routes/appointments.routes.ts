e78bde1465831649ee786b6a745c3adb195f83f3
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-11-22 14:04:40 -0300

    chore: Adapt appointments with provider_id

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -19,14 +19,15 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    // eslint-disable-next-line camelcase
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
 
     const createAppointment = new CreateAppointmentService();
 
     const appointment = await createAppointment.call({
-      provider,
+      provider_id,
       date: parsedDate,
     });
 
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,10 +1,11 @@
+/* eslint-disable camelcase */
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
@@ -13,7 +14,7 @@ interface Request {
  */
 
 class CreateAppointmentService {
-  public async call({ date, provider }: Request): Promise<Appointment> {
+  public async call({ date, provider_id }: Request): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
     const appointmentDate = startOfHour(date);
 
@@ -26,7 +27,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
