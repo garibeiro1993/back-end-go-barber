3c2f35333aefb7cae9b268d088b7f4e9459822be
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-11-30 08:29:33 -0300

    Import multer and upload config

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,24 +1,31 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json(user);
+  return response.json(user);
+});
+
 usersRouter.patch(
   '/avatar',
   ensureAuthenticated,
